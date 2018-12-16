5fa5ad95379c2f8e71e2b40438b6f5712e332395
tamdn <tamdn@runsystem.net>
2018-12-16 21:39:00 +0700

    Service call service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,21 @@ import { MoviesComponent } from './movies/movies.component';
 import { FormsModule } from '@angular/forms';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { MovieService } from './services/movie.service';
+import { MessageService } from './services/message.service';
+import { MessagesComponent } from './messages/messages.component';
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
-    MovieDetailComponent
+    MovieDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule
   ],
-  providers: [MovieService],
+  providers: [MovieService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -3,15 +3,18 @@ import { listMovies } from '../movies/template_data';
 import { Movie } from '../../models/movie';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(public messageService: MessageService) { }
 
   getMovies(): Observable<Movie[]> {
+    this.messageService.add(`Get Movies at: ${new Date().toLocaleString()}`);
+    console.log(this.messageService.messages);
     return of(listMovies);
   }
 }
